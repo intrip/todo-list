4c63235a2009a88349caea1a6587becb6c50a6bc
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2015-11-25 14:12:58 +0100

    - Created hierarchical list

--- app/assets/javascripts/directives/items.js
+++ app/assets/javascripts/directives/items.js
@@ -1,14 +1,14 @@
 angular.module('todoApp')
 
-.directive('items', function(){
+    .directive('items', function () {
         return {
-            replace: true,
+//            replace: true,
             restrict: 'E',
             templateUrl: 'todo/items.html',
-            controller: function($scope){
+            controller: function ($scope) {
                 $scope.items = [
                     {
-                        id: "1",
+                        id: 1,
                         title: "titolo1",
                         description: "description1",
                         body: "html text",
@@ -16,32 +16,32 @@ angular.module('todoApp')
                         completed: false,
                         created_at: "2015-11-24 12:40:06",
                         updated_at: "2015-11-24 12:40:06",
-                        due_date: "2015-11-24 12:40:06"
+                        due_date: "2015-11-24 12:40:06",
+                        items: [{
+                            id: 2,
+                            title: "titolo2",
+                            description: "description2",
+                            body: "html text",
+                            order: 2,
+                            completed: true,
+                            created_at: "2015-11-24 12:40:06",
+                            updated_at: "2015-11-24 12:40:06",
+                            due_date: "2015-11-24 12:40:06",
+                            items: [{
+                                id: 3,
+                                title: "titolo3",
+                                description: "description3",
+                                body: "html text",
+                                order: 3,
+                                completed: false,
+                                created_at: "2015-11-24 12:40:06",
+                                updated_at: "2015-11-24 12:40:06",
+                                due_date: "2015-11-24 12:40:06"
+                            }],
+                        }],
                     },
                     {
-                        id: "2",
-                        title: "titolo2",
-                        description: "description2",
-                        body: "html text",
-                        order: 2,
-                        completed: true,
-                        created_at: "2015-11-24 12:40:06",
-                        updated_at: "2015-11-24 12:40:06",
-                        due_date: "2015-11-24 12:40:06"
-                    },
-                    {
-                        id: "3",
-                        title: "titolo3",
-                        description: "description3",
-                        body: "html text",
-                        order: 3,
-                        completed: false,
-                        created_at: "2015-11-24 12:40:06",
-                        updated_at: "2015-11-24 12:40:06",
-                        due_date: "2015-11-24 12:40:06"
-                    },
-                    {
-                        id: "4",
+                        id: 4,
                         title: "titolo4",
                         description: "description4",
                         body: "html text",
@@ -52,8 +52,10 @@ angular.module('todoApp')
                         due_date: "2015-11-24 12:40:06"
                     }
 
-                ];
+                ]
+                ;
             }
-            //@jtodoIMP use parent_id and show hierarchy
-        };
-    });
+        }
+            ;
+    })
+;
