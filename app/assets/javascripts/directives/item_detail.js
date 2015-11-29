155934fd59398796537283b53bf0630f84dc3569
Jacopo <beschi.jacopo@gmail.com>
2015-11-29 12:28:32 +0100

    Removed nesteditem service and used nested items as default in angular

--- app/assets/javascripts/directives/item_detail.js
+++ app/assets/javascripts/directives/item_detail.js
@@ -1,14 +1,11 @@
 angular.module('todoApp')
 
-    .directive('itemDetail', function (nestedItems) {
+    .directive('itemDetail', function () {
         return {
 //            replace: true,
             restrict: 'E',
             templateUrl: 'todo/item-detail.html',
             link: function(scope, element, attrs) {
-                scope.buildNestedItems = function(){
-                    nestedItems.build(scope);
-                }
 
                 //TODO branch out in f-api, remove all the logit for hierarchy except the one to render that,
                 // then do the add new btn that add the item on top and calls the api after
--- app/assets/javascripts/directives/items.js
+++ app/assets/javascripts/directives/items.js
@@ -1,6 +1,6 @@
 angular.module('todoApp')
 
-    .directive('items', function(nestedItems) {
+    .directive('items', function () {
         return {
 //            replace: true,
             restrict: 'E',
@@ -9,43 +9,30 @@ angular.module('todoApp')
                 $scope.items = [
                     {
                         id: 1,
-                        parent_id: null,
                         title: "titolo1",
                         description: "description1",
                         body: "html text",
                         order: 1,
                         completed: false,
-                        created_at: "2015-11-24 12:40:06",
-                        updated_at: "2015-11-24 12:40:06",
-                        due_date: "2015-11-24 12:40:06"
-                    },
-                    {
-                        id: 2,
-                        parent_id: 1,
-                        title: "titolo2",
-                        description: "description2",
-                        body: "html text",
-                        order: 2,
-                        completed: true,
+                        items: [
+                            {
+                                id: 2,
+                                title: "titolo2",
+                                description: "description2",
+                                body: "html text",
+                                order: 2,
+                                completed: true,
+                                created_at: "2015-11-24 12:40:06",
+                                updated_at: "2015-11-24 12:40:06",
+                                due_date: "2015-11-24 12:40:06"
+                            }
+                        ],
                         created_at: "2015-11-24 12:40:06",
                         updated_at: "2015-11-24 12:40:06",
                         due_date: "2015-11-24 12:40:06"
                     },
                     {
                         id: 3,
-                        parent_id: 1,
-                        title: "titolo3",
-                        description: "description3",
-                        body: "html text",
-                        order: 3,
-                        completed: false,
-                        created_at: "2015-11-24 12:40:06",
-                        updated_at: "2015-11-24 12:40:06",
-                        due_date: "2015-11-24 12:40:06"
-                    },
-                    {
-                        id: 4,
-                        parent_id: null,
                         title: "titolo4",
                         description: "description4",
                         body: "html text",
@@ -55,15 +42,12 @@ angular.module('todoApp')
                         updated_at: "2015-11-24 12:40:06",
                         due_date: "2015-11-24 12:40:06"
                     }
-        ];
+                ];
 
-                $scope.selectItem = function(item) {
-                    // GO from here and find a way to also get the item in the list
+                $scope.selectItem = function (item) {
                     $scope.selectedItem = item;
-                    $scope.selectedItemFromItems = nestedItems.searchItem(item.id,$scope.items);
                 }
 
-                nestedItems.build($scope);
             }
         };
     });
